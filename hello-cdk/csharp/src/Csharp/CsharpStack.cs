4115e66a49b975ac70e58f2b9c8b4aacfcbc1fcb
Victor Manuel Islas Espinoza <islasvictorm@gmail.com>
2022-08-15 21:26:48 -0500

    initial commit

---
+++ hello-cdk/csharp/src/Csharp/CsharpStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Csharp
+{
+    public class CsharpStack : Stack
+    {
+        internal CsharpStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
---
+++ hello-cdk/csharp/src/Csharp/stacks/HelloCdkStack.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.CDK;
+using Amazon.CDK.AWS.S3;
+
+namespace Csharp.stacks
+{
+    public class HelloCdkStack : Stack
+    {
+        public HelloCdkStack(App scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            new Bucket(this, "MyFirstBucket", new BucketProps
+            {
+                Versioned = true
+            });
+        }
+    }
+}
